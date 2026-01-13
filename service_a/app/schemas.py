07d8da90c61035565108018072dead932bcabfaa
michael friedman <myklp1013@gmail.com>
2026-01-13 09:33:49 +0200

    adding class PostIpAndCoordinates

--- service_a/app/schemas.py
+++ service_a/app/schemas.py
@@ -2,7 +2,9 @@ from pydantic import BaseModel ,Field,IPvAnyAddress
 
 
 class PostIp(BaseModel):
-
     ip: IPvAnyAddress
     
+class PostIpAndCoordinates(BaseModel):
+    ip :IPvAnyAddress
+    coordinates : dict[str,float]
 
