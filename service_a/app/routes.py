4ab039c993e9c50fcd0d2acaeb2459bc69b7453c
elazar meijers <meijers212@gmail.com>
2026-01-13 09:20:12 +0200

    add basic router for service

---
+++ service_a/app/routes.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, APIRouter, HTTPException
+from typing import Optional
+
+
+
+
+
+router = APIRouter()
+
+
+@router.post("/")
+def Receiving_coordinates_and_sending_to_server_b():
+    pass
+    
+        
+
+
