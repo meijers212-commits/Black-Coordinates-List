3716605450cd463409eb69252b1b1712deb7f041
elazar meijers <meijers212@gmail.com>
2026-01-13 16:19:25 +0200

    add main file ehit aditinal nided

---
+++ service_b/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+import uvicorn
+from routes import redis_conactor_server
+
+app = FastAPI()
+
+app.include_router(redis_conactor_server, tags=["db_controler"])
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="localhost",port=8000)
