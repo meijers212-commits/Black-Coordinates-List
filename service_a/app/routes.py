77cfa2311bbf401bb0f3d8f8e90df3490b7cabd2
elazar meijers <meijers212@gmail.com>
2026-01-13 11:57:48 +0200

    add basic rauter to chec server and continue

--- service_a/app/routes.py
+++ service_a/app/routes.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI, APIRouter, HTTPException
 from schemas import PostIp
+from services import GetCoordinates as gc
 
 
 
+ip_router = APIRouter()
 
-router = APIRouter()
 
-
-@router.post("/ip")
+@ip_router.post("/ip")
 def client_api_manegment(ip:PostIp):
-    
-    
+    coordinates = gc.get_lat_lon(gc.ip_request(ip=ip))
+    return coordinates
+
         
 
 
