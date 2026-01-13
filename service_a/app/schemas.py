73a33af4200c0cbe52f483dea29aab506961ab1e
michael friedman <myklp1013@gmail.com>
2026-01-13 10:01:25 +0200

    adding class GetRequests

--- service_a/app/schemas.py
+++ service_a/app/schemas.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel ,Field,IPvAnyAddress
-
+from pydantic import BaseModel ,IPvAnyAddress
+from typing import Optional
 
 class PostIp(BaseModel):
     ip: IPvAnyAddress
@@ -8,3 +8,7 @@ class PostIpAndCoordinates(BaseModel):
     ip :IPvAnyAddress
     coordinates : dict[str,float]
 
+class GetRequests(BaseModel):
+    ip :IPvAnyAddress
+    coordinates : dict[str,float]
+    description: Optional[str] = None
