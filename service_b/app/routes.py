625fd675a9e52e916891367beb5f708290a94892
elazar meijers <meijers212@gmail.com>
2026-01-13 16:14:32 +0200

    add two routers 'get_all' and 'writ to db'

---
+++ service_b/app/routes.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from schemas import PostIpAndCoordinates, PostRequests
+
+
+
+redis_conactor_server = APIRouter()
+
+
+@redis_conactor_server.post("/save_to_db")
+def save_to_data_base():
+    pass
+   
+ 
+@redis_conactor_server.get("/get_all")
+def get_all_from_db():
+    pass
+
+
